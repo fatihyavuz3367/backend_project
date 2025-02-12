8f24bcab46beb62cd82aa2efa0a3a31988c555c6
fatih yavuz <fatihf354@gmail.com>
2025-02-12 12:45:21 +0300

    12.02.2025

---
+++ src/main/java/com/techproed/schoolmanagementbackendb326/service/user/UserService.java
@@ -0,0 +1,31 @@
+package com.techproed.schoolmanagementbackendb326.service.user;
+
+import com.techproed.schoolmanagementbackendb326.payload.request.user.UserRequest;
+import com.techproed.schoolmanagementbackendb326.payload.response.business.ResponseMessage;
+import com.techproed.schoolmanagementbackendb326.payload.response.user.UserResponse;
+import com.techproed.schoolmanagementbackendb326.service.validator.UniquePropertyValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private  final UniquePropertyValidator uniquePropertyValidator;
+
+    public ResponseMessage<UserResponse> saveUser(@Valid UserRequest userRequest, String userRole) {
+        //validate unique prop.
+        uniquePropertyValidator.checkDuplication(
+                userRequest.getUsername(),
+                userRequest.getSsn(),
+                userRequest.getPhoneNumber(),
+                userRequest.getEmail()
+        );
+        //DTO->entity mapping
+
+    }
+
+
+}
